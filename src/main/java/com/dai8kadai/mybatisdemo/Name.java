23274d623a35b81232c26ad3f5b56058215996a3
aki-27to <120763272+aki-27to@users.noreply.github.com>
2023-01-28 18:54:03 +0900

    SETTINGS

---
+++ src/main/java/com/dai8kadai/mybatisdemo/Name.java
@@ -0,0 +1,19 @@
+package com.dai8kadai.mybatisdemo;
+
+public class Name {
+   private int id;
+   private String Name;
+
+    public Name(int id, String name) {
+        this.id = id;
+        Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+}
